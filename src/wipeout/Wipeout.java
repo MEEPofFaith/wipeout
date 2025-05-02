35ab76a74782b4ac7631264a8ba2411bf87e6577
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-05-02 11:53:37 -0700

    Setup

---
+++ src/wipeout/Wipeout.java
@@ -0,0 +1,17 @@
+package wipeout;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class Wipeout extends Mod{
+    public Wipeout(){
+    }
+
+    @Override
+    public void init(){
+        super.init();
+    }
+}
